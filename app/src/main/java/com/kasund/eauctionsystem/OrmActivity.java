ac93d25079bc5c635b17fe3d5c56ad4fc8f4af8c
L213Dragon <l213Dragon@hotmail.com>
2016-08-13 05:40:21 +0300

    AndroidAppfor Education

---
+++ app/src/main/java/com/kasund/eauctionsystem/OrmActivity.java
@@ -0,0 +1,24 @@
+package com.kasund.eauctionsystem;
+
+import com.kasund.eauctionsystem.db.DatabaseHelper;
+
+public class OrmActivity extends BaseActivity {
+
+	private DatabaseHelper databaseHelper = null;
+
+	public DatabaseHelper getDBHelper() {
+		if (databaseHelper == null) {
+			databaseHelper = OrmHelperManager.getHelper(TAG, this, DatabaseHelper.class);
+		}
+		return databaseHelper;
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (databaseHelper != null) {
+			OrmHelperManager.releaseHelper(TAG);
+			databaseHelper = null;
+		}
+	}
+}
